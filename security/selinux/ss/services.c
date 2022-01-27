91d964ce027292849f484984fab390b4642c1d79
Dylan Neve <dylanneve1@gmail.com>
2022-01-27 13:21:11 +0000

    selinux: Enable auditing
    This reverts commit 597c831b5e7b386eb8ad588cd0d3c7e366564cfe.
    
    Revert "selinux: Remove audit dependency"
    
    This reverts commit 9bff51de282959510ca3c524b43f4d6d81615a3a.
    
    Revert "derp:selinux: don't require auditing"
    
    This reverts commit ecae2f02f93da9c379edf127bf00f08f6ee39187.
    
    Revert "selinux: avc: omit audit callbacks if disabled"
    
    This reverts commit 4f65073db5db8508f95058625d9bf13455645f5c.
    
    Revert "selinux: Allow audit to be disabled"
    
    This reverts commit a884dc9c36c92a3d2d73f4340113903098f56095.
    
    Revert "selinux: don't require auditing"
    
    This reverts commit e410ae8ab91925c46f4c4122089bbd689742a930.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3302,7 +3302,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3325,7 +3324,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
